834a857852d22f5dc801f6433a7e31d9a9ad730e
karpenko <karpenko@reshenie-soft.ru>
2022-02-06 22:32:46 +0300

    Add model and attribute;

--- src/main/java/com/se4oev/spring/mvc/MyController.java
+++ src/main/java/com/se4oev/spring/mvc/MyController.java
@@ -1,7 +1,10 @@
 package com.se4oev.spring.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by se4oev on 06.02.2022
@@ -20,8 +23,21 @@ public class MyController {
         return "ask-emp-details-view";
     }
 
+//    @RequestMapping("/showDetails")
+//    public String showEmpDetails() {
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    public String showEmpDetails() {
+    public String showEmpDetails(HttpServletRequest request, Model model) {
+
+        String empName = request.getParameter("employeeName");
+        empName = "Mr. " + empName;
+
+        model.addAttribute("nameAttribute", empName);
+
+        model.addAttribute("description", " - udemy instructor");
+
         return "show-emp-details-view";
     }
 
